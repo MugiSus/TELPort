53daa3968a2a6a9c7a1dee3fedcdbb3c97e42617
MugiSus <42643211+MugiSus@users.noreply.github.com>
2021-11-02 16:44:04 +0900

    Update src.js

--- app/src.js
+++ app/src.js
@@ -1,5 +1,5 @@
 const FFTsize = 4096;
-const FirstFreuency = (44100 / FFTsize) * 55;
+const FirstFreuency = (44100 / FFTsize) * 60;
 const BytesPerRound = 28;
 const Frequencies = new Array(8 * BytesPerRound).fill(0).map((_, i) => {
     return FirstFreuency + (44100 / FFTsize * 5) * i;
