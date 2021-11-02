4b698f045d229ba8f7ce344d1ce88ca2948ce97d
MugiSus <42643211+MugiSus@users.noreply.github.com>
2021-11-02 17:30:46 +0900

    Update src.js

--- app/src.js
+++ app/src.js
@@ -21,7 +21,7 @@ const AudioContext = window.AudioContext || window.webkitAudioContext; // both (
 let context; // both
 
 const StartingSoundSpeed = 500; // both // milliseconds
-let speed = 145; // both // milliseconds 
+let speed = 130; // both // milliseconds 
 
 let requestAnimationFrameID, lastCallbackTime; // listen
 let intervalID; // call
